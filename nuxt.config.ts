594ed8da1bb7a25225a39a31f17c8a056e502ca9
Soslan_ZS <soslan_zs@mail.ru>
2026-09-10 16:26:15 +0300

    Configure GitHub Pages deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@
 export default defineNuxtConfig({
 	compatibilityDate: '2024-04-03',
 	devtools: { enabled: true },
+	nitro: {
+		preset: 'github-pages',
+	},
 	css: ['reset-css/reset.css', '~/assets/scss/main.scss'],
 	app: {
 		head: {
